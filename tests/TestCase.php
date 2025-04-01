2b5eeb78fbfc99ec27ce4a769569735d4d2f440b
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-04-01 18:07:03 +0200

    Remove unnecessary constructor from TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,11 +11,6 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-	public function __construct(?string $name = null, array $data = [], $dataName = '')
-	{
-		parent::__construct($name, $data, $dataName);
-	}
-
 	public function container(): Container
 	{
 		$container = new Container();
