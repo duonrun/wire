b8af1a4e1af4036c2c0f133c696b4165cf1e8f1e
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-12 12:53:37 +0200

    Add mago.toml, remove .php-cs-fixer config

--- .php-cs-fixer.dist.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$finder = PhpCsFixer\Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/docs/code']);
-$config = new Duon\Development\PhpCsFixer\Config();
-
-return $config->setFinder($finder);
